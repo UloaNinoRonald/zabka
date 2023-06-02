b929813604be80f27183f92d1465c36c5e12af22
Ronald Uloa Nino <ronalduloanino@gmail.com>
2023-06-02 19:30:48 +0200

    leftMenu

--- componentJs/nav.js
+++ componentJs/nav.js
@@ -1,29 +1,63 @@
 const navigation = document.createElement("div");
-
 navigation.classList.add("navigation");
-
 boxContainer.appendChild(navigation);
 
 const logo = document.createElement("div");
-
 logo.classList.add("logo");
-
 navigation.appendChild(logo);
 
 const nav = document.createElement("div");
-
 nav.classList.add("nav");
-
 navigation.appendChild(nav);
+
+const logotype = document.createElement("img");
+logotype.classList.add("logotype");
+logo.appendChild(logotype);
+logotype.src = "./image/logo.png";
+
+const logistic = document.createElement("h2");
+logistic.classList.add("logistic");
+logo.appendChild(logistic);
+logistic.textContent = "LOGISTIC POLSKA";
 
 const ul = `
     <ul>
-        <li><a href = ""><p>fvdf1</p></a></li>
-        <li><a href = ""><p>2dfg<p></a></li>
-        <li><a href = ""><p>3fgv<p></a></li>
-        <li><a href = ""><p>4ergerg<p></a></li>
-        <li><a href = ""><p>5cergsf<p></a></li>
-        <li><a href = ""><p>6srfg<p></a></li>
+        <li>
+                <a href = "">
+                    <p>Главная</p>
+                </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>Мрожня</p>
+                 </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>Хлодня</p>
+                 </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>Food</p>
+                 </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>ФОлия</p>
+                 </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>Мойка</p>
+                 </a>
+        </li>
+        <li>
+                 <a href = "">
+                    <p>акциза</p>
+                 </a>
+        </li>
+        
     </ul>
 `;
 
